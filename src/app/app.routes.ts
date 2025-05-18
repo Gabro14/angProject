54c14390adfa7b6808a2664afcef8cb1ee9dcb44
Default <xbotandspacemorty@gmail.com>
2025-05-18 21:50:06 +0400

    rame

---
+++ src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
+import { DetailsComponent } from './details/details.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { MenuComponent } from './menu/menu.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+
+    {path: '', redirectTo:  'home', pathMatch: "full"},
+    {path: "home", component: HomeComponent},
+    {path: "menu", component: MenuComponent},
+    {path: "about", component: AboutComponent},
+    {path: "details/:id", component: DetailsComponent},
+    {path: "roomDetails/:id", component: RoomDetailsComponent},
+    {path: "register", component: RegisterComponent},
+    {path: "login", component: LoginComponent},
+    {path: "**", component: ErrorComponent}
+];
